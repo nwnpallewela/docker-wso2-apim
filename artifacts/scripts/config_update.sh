868f8ea1bbf7ab9971a8c44152d01738c20df713
nwnpallewela <nwnpallewela@gmail.com>
2016-12-07 22:19:10 +0530

    Adding comment

--- artifacts/scripts/config_update.sh
+++ artifacts/scripts/config_update.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 #This script should change the copnfiguration files according to the environment variables
 if [ -z "$ANALYTICS" ]; then
     echo "[INFO]Analytics is not enabled"
 else
-    echo "[INFO]Analytics is enabled"
+    echo "[INFO]Enabling Analytics"
     rm /opt/wso2am-2.0.0/repository/conf/api-manager.xml
     mv /opt/docker-wso2-apim-master/artifacts/configs/single_node_am_with_analytics/api-manager.xml /opt/wso2am-2.0.0/repository/conf/api-manager.xml && echo "[INFO]Analytics is enabled"
 
 fi
-sh bash
-#sh /opt/wso2am-2.0.0/bin/wso2server.sh
+sh /opt/wso2am-2.0.0/bin/wso2server.sh
