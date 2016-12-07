0613c43c3eb55a3f4f3b49a78ead2c26645cabe7
nwnpallewela <nwnpallewela@gmail.com>
2016-12-07 21:52:48 +0530

    Add echo to config update

--- artifacts/scripts/config_update.sh
+++ artifacts/scripts/config_update.sh
@@ -2,6 +2,7 @@
 if [ -z "$ANALYTICS" ]; then
     echo "[INFO]Analytics is not enabled"
 else
+    echo "[INFO]Analytics is enabled"
     rm /opt/wso2am-2.0.0/repository/conf/api-manager.xml
     mv /opt/docker-wso2-apim-master/artifacts/configs/single_node_am_with_analytics/api-manager.xml /opt/wso2am-2.0.0/repository/conf/api-manager.xml && echo "[INFO]Analytics is enabled"
 
